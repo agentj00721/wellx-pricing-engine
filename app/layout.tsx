da4496d05194416598a79eec992385e639dd3b39
agentj00721 <agentj00721@gmail.com>
2026-05-27 13:27:05 +0400

    fix: add Wellx SVG favicon link tags to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,8 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <head>
+                <link rel="icon" href="/wellx-icon.svg" type="image/svg+xml" />
+                <link rel="apple-touch-icon" href="/wellx-icon.svg" />
         <script
           dangerouslySetInnerHTML={{
             __html: `(() => {
